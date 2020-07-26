a756517f7c55ed9af4a57355bd7a3816b564e99f
sushantr5 <sushantr5@gmail.com>
2020-07-26 16:03:11 +0200

    More adjustments

--- src/mgos_neopixel.c
+++ src/mgos_neopixel.c
@@ -84,12 +84,24 @@ void mgos_neopixel_show(struct mgos_neopixel *np) {
   mgos_gpio_write(np->pin, 0);
   mgos_usleep(300);
 #if MGOS_ENABLE_BITBANG
+  LOG(LL_INFO, ("\nInside BitBang\n"));
+  cs_log_printf("\nInside BitBang\n");
   mgos_bitbang_write_bits(np->pin, MGOS_DELAY_100NSEC, 3, 8, 8, 8, np->data,
                           np->num_pixels * NUM_CHANNELS);
 #endif
   mgos_gpio_write(np->pin, 0);
   mgos_usleep(300);
   mgos_gpio_write(np->pin, 1);
+  
+#if MGOS_ENABLE_BITBANG
+/*  uint8_t data = 0;
+  mgos_bitbang_write_bits(np->pin, MGOS_DELAY_USEC, -1, 30, -1, 30, &data, 1);
+  mgos_bitbang_write_bits(np->pin, MGOS_DELAY_100NSEC, 3, 9, 9, 6, np->data,
+                          np->num_pixels * NUM_CHANNELS);
+  mgos_bitbang_write_bits(np->pin, MGOS_DELAY_USEC, -1, 30, -1, 30, &data, 1);
+  mgos_bitbang_write_bits(np->pin, MGOS_DELAY_USEC, 30, -1, 30, -1, &data, 1);*/
+#endif
+
 }
 
 void mgos_neopixel_free(struct mgos_neopixel *np) {
